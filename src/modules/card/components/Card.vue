6d8552d23a09a163fbd8b9b3fb1852e71dd13394
cristian.cdi@gmail.com <Neverwinter2>
2019-06-18 23:28:20 -0300

    First Commit

---
+++ src/modules/card/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+
+            <v-card :elevation="elevation" :hover="hover" :class="{ 'pcard-indigo': true, 'pbox': true }">
+                <v-card-title primary-title>
+                    <v-layout row>
+                        <v-flex xs10>
+                            <div>
+                                <h3 class="pcard-indigo ptitle display-1 font-weight-black font-italic">{{title}}</h3>
+                                <div> {{ subtitle }}</div>
+                            </div>
+
+                        </v-flex>
+                        <v-flex xs2>
+                            <v-img
+                                    :src="require('../../../assets/dba.png')"
+
+                            />
+                        </v-flex>
+                    </v-layout>
+
+                </v-card-title>
+
+            </v-card>
+
+</template>
+
+<script>
+    export default {
+        name: "Card",
+        props: {
+            title: String,
+            subtitle: String,
+        },
+        data: function () {
+            return {
+                elevation: 10,
+                hover: 10
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+    .pcard-indigo.pbox {
+        border: 8px #303F9F solid;
+
+    }
+
+    .pcard-indigo.ptitle{
+        color:  #1A237E;
+    }
+</style>
---
+++ src/modules/card/page/Cards.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-container fluid  grid-list-xl>
+            <v-layout row wrap>
+                <v-flex xs4>
+                    <card :title="'DBA'" :subtitle="'Administrador de base de datos'"></card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+    </div>
+</template>
+
+<script>
+    import Card from '../components/Card'
+    export default {
+        name: "Cards",
+        components:{Card}
+    }
+</script>
+
+<style scoped>
+
+</style>
